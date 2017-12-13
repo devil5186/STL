8cf85af18b4a08cb4a1703efea0a362e960a5840
xlin <xlin@stu.xidian.edu.cn>
2017-12-13 21:20:02 +0800

    Vector

---
+++ vector.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <iostream>
+int main(int argc, char const *argv[])
+{
+	int inta[]={5,4,0,0,7};
+	std::cout<<sizeof inta<<std::endl;
+	std::vector<int> vec(inta,inta+5);
+	for(int a:vec){
+		std::cout<<a<<std::endl;
+	}
+	return 0;
+}
